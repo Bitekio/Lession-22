433f760ae74aba64e2f446d2ece4ecebaae2225e
Bitekio <a-tleubaev@bk.ru>
2024-02-12 22:14:19 +0600

    Initial commit

---
+++ task.py
@@ -0,0 +1,15 @@
+"""
+Homework
+"""
+
+
+from docx import Document
+
+def create_word_file(text):
+    doc = Document()
+    doc.add_paragraph(text)
+    doc.save('output.docx')
+
+if __name__ == "__main__":
+    user_input = input("Введите текст для создания Word-файла: ")
+    create_word_file(user_input)
